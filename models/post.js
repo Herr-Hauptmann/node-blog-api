9da7b2d49f7cede320e615c7a40a91beae619bef
Herr-Hauptmann <tarik.horoz@gmail.com>
2022-12-11 22:30:27 +0100

    add field validation

--- models/post.js
+++ models/post.js
@@ -7,34 +7,47 @@ module.exports = (sequelize, DataTypes) => {
          * This method is not a part of Sequelize lifecycle.
          * The `models/index` file will call this method automatically.
          */
-        static associate({Comment}) {
-            this.hasMany(Comment, {foreignKey: "id", onDelete: "CASCADE"});
+        static associate({ Comment }) {
+            this.hasMany(Comment, { foreignKey: "id", onDelete: "CASCADE" });
         }
-        toJSON(){
-            return {...this.get(), id: undefined};
+        toJSON() {
+            return { ...this.get(), id: undefined };
         }
     }
     Post.init(
         {
             slug: {
-                type:DataTypes.STRING,
+                type: DataTypes.STRING,
                 allowNull: false,
                 unique: true,
-                validate:{
-                    notEmpty:true,
-                }
+                validate: {
+                    notEmpty: { msg: "Slug can't be empty" },
+                    notNull: { msg: "Slug can't be null" },
+                },
             },
             title: {
-                type:DataTypes.STRING,
+                type: DataTypes.STRING,
                 allowNull: false,
+                validate: {
+                    notEmpty: { msg: "Title can't be empty" },
+                    notNull: { msg: "Title can't be null" },
+                },
             },
             description: {
-                type:DataTypes.STRING,
+                type: DataTypes.STRING,
                 allowNull: false,
+                validate: {
+                    notEmpty: { msg: "Description can't be empty" },
+                    notNull: { msg: "Description can't be null" },
+                },
             },
             body: {
-                type:DataTypes.TEXT,
+                type: DataTypes.TEXT,
                 allowNull: false,
+                validate: {
+                    notEmpty: { msg: "Body can't be empty" },
+                    notNull: { msg: "Body can't be null" },
+                },
             },
         },
         {
--- routes/posts.js
+++ routes/posts.js
@@ -11,9 +11,9 @@ POSTS
 router.get("/", async (req, res) => {
     try {
         const posts = await Post.findAll({
-            order: [ ['createdAt', 'DESC'], ],
+            order: [["createdAt", "DESC"]],
         });
-        return res.json({ "blogPosts": posts, "postsCount" : posts.length});
+        return res.json({ blogPosts: posts, postsCount: posts.length });
     } catch (err) {
         return res.status(500).json({ error: "Greška pri dobavljanju postova." });
     }
@@ -50,28 +50,26 @@ router.get("/:slug", async (req, res) => {
 
 router.delete("/:slug", async (req, res) => {
     const slug = req.params.slug;
-    try{
-        const postId = (await Post.findOne({
-            where: {slug}
-        })).id;
+    try {
+        const postId = (
+            await Post.findOne({
+                where: { slug },
+            })
+        ).id;
         //Firstly we delete all of the comments associated with the post, no need to await, we don't care when it's done
         Comment.destroy({
-            where : {postId}
+            where: { postId },
         });
 
         const done = await Post.destroy({
-            where: {slug}
+            where: { slug },
         });
-        if (done)
-            return res.json({"msg" : "Post uspjesno obrisan"});
-        else
-            return res.json({"msg" : "Post ne postoji!"});
-    }
-    catch (err) {
+        if (done) return res.json({ msg: "Post uspjesno obrisan" });
+        else return res.json({ msg: "Post ne postoji!" });
+    } catch (err) {
         return res.status(500).json({ error: err });
     }
 });
-
 
 /*
 
@@ -84,50 +82,47 @@ router.post("/:slug/comments", async (req, res) => {
     let { body } = req.body.comment;
     try {
         const post = await Post.findOne({ where: { slug } });
-        const comment = await Comment.create(
-            { 
-                body: body, 
-                postId: post.id 
-            });
+        const comment = await Comment.create({
+            body: body,
+            postId: post.id,
+        });
         return res.json({ comment: comment });
     } catch (err) {
         return res.status(500).json(err);
     }
 });
 
-router.get("/:slug/comments", async(req,res)=>{
-    
+router.get("/:slug/comments", async (req, res) => {
     const slug = req.params.slug;
-    try{
-        const postId = (await Post.findOne({where: {slug}})).id;
-        const comments = await Comment.findAll({where:{
-            postId
-        }});
-        return res.json({commnets: comments});
-    }catch(err)
-    {
+    try {
+        const postId = (await Post.findOne({ where: { slug } })).id;
+        const comments = await Comment.findAll({
+            where: {
+                postId,
+            },
+        });
+        return res.json({ commnets: comments });
+    } catch (err) {
         return res.status(500).json(err);
     }
 });
 
-router.delete("/:slug/comments/:id", async(req,res)=>{
+router.delete("/:slug/comments/:id", async (req, res) => {
     const slug = req.params.slug;
     const id = req.params.id;
-    try{
-        const postId = (await Post.findOne({where: {slug}})).id;
-        const done = await Comment.destroy({where:{
-            postId,
-            id
-        }});
-        if (done)
-            return res.json({"msg" : "Komentar uspjesno izbrisan"})
-            else
-            return res.json({"msg" : "Komentar ne postoji!"})
-    }catch(err)
-    {
+    try {
+        const postId = (await Post.findOne({ where: { slug } })).id;
+        const done = await Comment.destroy({
+            where: {
+                postId,
+                id,
+            },
+        });
+        if (done) return res.json({ msg: "Komentar uspjesno izbrisan" });
+        else return res.json({ msg: "Komentar ne postoji!" });
+    } catch (err) {
         return res.status(500).json(err);
     }
 });
-
 
 module.exports = router;
